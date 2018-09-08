9b52b9d18b5128db4553507dac36883b18417e03
Julio Sene França <julio.sene@gmail.com>
2018-09-08 15:15:26 -0300

    Stable php7.2 version

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script will install Nginx + PHP 7.1
 cd ~
-sudo -i
-apt-get update
-# apt-get -fy dist-upgrade
-apt-get -fy upgrade
+# sudo -i
+sudo apt-get update && sudo apt-get -fy upgrade
+
+sudo locale-gen "en_US.UTF-8"
 apt-get install -fy lsb-release bc
 REL=`lsb_release -sc`
 DISTRO=`lsb_release -is | tr [:upper:] [:lower:]`
@@ -13,8 +13,6 @@ WORKER=`bc -l <<< "4*$NCORES"`
 
 AppToInstall=${1:-"none"}
 # wordpres, joomla, drupal
-PHPVersion=${2-7}
-
 
 InstallTools=${3:-"no"}
 ToolsUser=$4
@@ -41,18 +39,24 @@ apt-get -y install nfs-common
 # apt-get install -y -f cifs-utils
 
 # Remove Apache2 before install NginX
-apt-get remove -fy apache2
+# apt-get remove -fy apache2
 
 #Install NginX
 apt-get install -fy nginx
 
 # # PHP 7.2
 apt-get install -fy php7.2
-apt-get install -fy php-pear php7.2-fpm php7.2-mysql php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
+apt-get install -fy php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
+# apt-get install -fy php-pear php7.2-fpm php7.2-mysql php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
+# Memcache client installation
+apt-get install -fy php7.2-memcached
 
 apt-get --purge autoremove -y
 # replace www-data to nginx into /etc/php/7.0/fpm/pool.d/www.conf
 sed -i 's/www-data/nginx/g;s/\;request_terminate_timeout = 0/request_terminate_timeout = 300/g;' /etc/php/7.2/fpm/pool.d/www.conf
+
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 100M/g;s/post_max_size = 8M/post_max_size = 64M/g;s/max_execution_time = 30/max_execution_time = 300/g" /etc/php/7.2/fpm/php.ini
+
 service php7.2-fpm restart
 
 # backup default Nginx configuration
@@ -62,27 +66,19 @@ cp /etc/nginx/nginx.conf /etc/nginx/nginx-conf.old
 #
 # Replace nginx.conf
 #
-wget https://raw.githubusercontent.com/juliosene/azure-nginx-php-mariadb-cluster/master/files/nginx.conf
+wget https://raw.githubusercontent.com/juliosene/Wordpress-AWS-/master/files/nginx.conf
 
 sed -i "s/#WORKER#/$WORKER/g" nginx.conf
 mv nginx.conf /etc/nginx/
 
 # replace Nginx default.conf
 #
-wget https://raw.githubusercontent.com/juliosene/azure-nginx-php-mariadb-cluster/master/files/default.conf
-
-
-sed -i "s,/var/run/php5-fpm.sock,/var/run/php/php7.2-fpm.sock,g" default.conf
-
-
-#sed -i "s/#WORKER#/$WORKER/g" nginx.conf
+wget https://raw.githubusercontent.com/juliosene/Wordpress-AWS-/master/files/default.conf
+# sed -i "s,/var/run/php5-fpm.sock,/var/run/php/php7.2-fpm.sock,g" default.conf
 mv default.conf /etc/nginx/conf.d/
 
-# Memcache client installation
-apt-get install -fy php7.2-memcached
-# wget https://raw.githubusercontent.com/juliosene/azure-nginx-php-mariadb-cluster/master/files/memcache.ini
-# mv memcache.ini /etc/php/mods-available/
-# ln -s /etc/php/mods-available/memcache.ini  /etc/php/7.0/fpm/conf.d/20-memcache.ini
+
+
 
 
 if [ $AppToInstall == "wordpress" ];
@@ -122,7 +118,25 @@ else
 #
 #mv /usr/share/nginx/html/index.html /usr/share/nginx/html/index.php
 mkdir /usr/share/nginx/html/web
-echo -e "<html><title>Azure Nginx PHP</title><body><h2 align='center'>Your Nginx and PHP are running!</h2><h2 align='center'>Host: <?= gethostname() ?></h2></br>\n<?php\nphpinfo();\n?></body>" > /usr/share/nginx/html/web/index.php
+#echo -e "<html><title>Azure Nginx PHP</title><body><h2 align='center'>Your Nginx and PHP are running!</h2><h2 align='center'>Host: <?= gethostname() ?></h2></br>\n<?php\nphpinfo();\n?></body>" > /usr/share/nginx/html/web/index.php
+
+cat > /usr/share/nginx/html/web/index.php << _EOF_
+<html>
+<head>
+    <title>Nginx PHP</title>
+</head>
+
+<body>
+   <h2 align='center'>Your Nginx and PHP are running!</h2>
+   <h2 align='center'>Host: <?= gethostname() ?></h2>
+   </br>
+   <?php
+      phpinfo();
+   ?>
+</body>
+
+</html>
+_EOF_
 
 fi
 
@@ -148,13 +162,6 @@ fi
 #
 # Services restart
 #
-if [ "$PHPVersion" -eq 7 ]; then
-sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 100M/g;s/post_max_size = 8M/post_max_size = 64M/g;s/max_execution_time = 30/max_execution_time = 300/g" /etc/php/7.0/fpm/php.ini
-service php7.0-fpm restart
-else
-sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 100M/g;s/post_max_size = 8M/post_max_size = 64M/g;s/max_execution_time = 30/max_execution_time = 300/g" /etc/php5/fpm/php.ini
-service php5-fpm restart
-fi
 
 chown -R nginx.nginx /usr/share/nginx/html/web
 
