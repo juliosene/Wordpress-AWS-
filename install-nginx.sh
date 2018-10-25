d0183a896034eb1c23c0939a5184f434ed430885
Julio Sene França <julio.sene@gmail.com>
2018-10-24 21:23:40 -0300

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -2,9 +2,12 @@
 # This script will install Nginx + PHP 7.1
 cd ~
 # sudo -i
+sudo apt-get install language-pack-UTF-8
+sudo locale-gen UTF-8
+# sudo locale-gen "en_US.UTF-8"
+
 apt-get install -fy python-software-properties
 LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
-sudo locale-gen "en_US.UTF-8"
 
 sudo apt-get update && sudo apt-get -fy upgrade
 
