ebecb86f02f4aa1c70a44e75ad107daaf4b30b8b
Julio Sene França <julio.sene@gmail.com>
2018-12-26 18:27:19 -0200

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -59,7 +59,8 @@ apt-get install -fy php7.2-memcached
 
 apt-get --purge autoremove -y
 # replace www-data to nginx into /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/www-data/nginx/g;s/\;request_terminate_timeout = 0/request_terminate_timeout = 300/g;' /etc/php/7.2/fpm/pool.d/www.conf
+#sed -i 's/www-data/nginx/g;'
+sed -i 's/\;request_terminate_timeout = 0/request_terminate_timeout = 300/g;' /etc/php/7.2/fpm/pool.d/www.conf
 
 sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 100M/g;s/post_max_size = 8M/post_max_size = 64M/g;s/max_execution_time = 30/max_execution_time = 300/g" /etc/php/7.2/fpm/php.ini
 
